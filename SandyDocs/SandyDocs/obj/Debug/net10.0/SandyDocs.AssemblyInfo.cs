99c9695c7b1627653b4e508745e544f9dbac8388
Eric-Soto <ericsoto926@gmail.com>
2026-09-13 14:05:56 -0400

    random files trying to know what's going on

--- SandyDocs/SandyDocs/obj/Debug/net10.0/SandyDocs.AssemblyInfo.cs
+++ SandyDocs/SandyDocs/obj/Debug/net10.0/SandyDocs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SandyDocs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77276b8bebab8e900df839b9f953d92ca1c07993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910e5ea47716cd2540b05f942722f11818b5a261")]
 [assembly: System.Reflection.AssemblyProductAttribute("SandyDocs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SandyDocs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
